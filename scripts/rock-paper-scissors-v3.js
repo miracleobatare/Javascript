cd479b0ef3ff2af049b57919de24aed45be31b15
Miracle Obatare <miracleobatare234@gmail.com>
2025-02-28 08:02:43 +0000

    Learning JavaScript

---
+++ scripts/rock-paper-scissors-v3.js
@@ -0,0 +1,133 @@
+let gameScore= JSON.parse(localStorage.getItem('gameScore')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+updateScoreElement();
+
+document.querySelector('.js-rock-button')
+    .addEventListener('click', () => {
+    playGame('rock');
+});
+document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+    playGame('paper');
+});
+document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+    playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+    if (event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key === 's') {
+        playGame('scissors');
+    }
+});
+
+function playGame(playMove) {
+    const computerMove = pickComputerMove();
+
+    let result = '';
+    if (playMove === 'rock') {
+        if (computerMove === 'rock') {
+            result = 'Tie!';
+        } else if (computerMove === 'paper') {
+            result = 'You Lose!';
+        } else if (computerMove === 'scissors') {
+            result = 'You Win!';
+        }
+    } else if (playMove === 'paper') {
+        if (computerMove === 'paper') {
+            result = 'Tie!';
+        } else if (computerMove === 'scissors') {
+            result = 'You Lose!';
+        } else if (computerMove === 'rock') {
+            result = 'You Win!';
+        }
+
+    } else if (playMove === 'scissors') {
+        if (computerMove === 'scissors') {
+            result = 'Tie!';
+        } else if (computerMove === 'rock') {
+            result = 'You Lose!';
+        } else if (computerMove === 'paper') {
+            result = 'You Win!'
+        }
+    }
+
+    if (result === 'You Win!') {
+        gameScore.wins += 1;
+    } else if (result === 'You Lose!') {
+        gameScore.losses += 1;
+    } else if (result === 'Tie!') {
+        gameScore.ties += 1;
+    }
+
+    localStorage.setItem('gameScore', JSON.stringify(gameScore));
+
+    updateScoreElement();
+
+    document.querySelector('.js-result').innerHTML = result;
+
+    document.querySelector('.js-moves').innerHTML = `You
+        <img class="move-icon" src="assets/${playMove}-emoji.png"></img>
+        <img class="move-icon" src="assets/${computerMove}-emoji.png"></img>
+        Computer`;
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score')
+        .innerHTML = `Wins: ${gameScore.wins}, Losses: ${gameScore.losses}, Ties: ${gameScore.ties}`;
+}
+
+
+function pickComputerMove() {
+    let computerMove = ''
+    const randomNumber = Math.random();
+
+    if (randomNumber >= 0 && randomNumber < 1/3){
+        computerMove ='rock';
+    }
+    else if (randomNumber >= 1/3 && randomNumber < 2/3){
+        computerMove = 'paper';
+    }
+    else if (randomNumber >= 2/3 && randomNumber < 1){
+        computerMove = 'scissors';
+    }
+    return computerMove;
+}
+let isAutoPlaying = false;
+let intervalID;
+
+document.querySelector('.js-auto-play')
+    .addEventListener('click', () => {
+        autoplay()
+    });
+
+function autoplay() {
+    if (!isAutoPlaying) {
+          intervalID =  setInterval( ()=> {
+            const playerMove = pickComputerMove();
+            playGame(playerMove);
+        }, 1000);
+        isAutoPlaying = true;
+    }
+    else {
+        clearInterval(intervalID);
+        isAutoPlaying = false;
+    }
+}
+
+document.querySelector('.js-reset')
+    .addEventListener('click', () => {
+        gameScore.wins = 0;
+        gameScore.losses = 0;
+        gameScore.ties = 0;
+        localStorage.removeItem('gameScore')
+})
+
--- scripts/todo-list-v3.js
+++ scripts/todo-list-v3.js
@@ -5,19 +5,22 @@ function renderTodoList () {
     let todoListHTML = ''
     ;
 
-    todoList.forEach(function (todoObject, index ){
+    todoList.forEach((todoObject, index ) => {
         const {name, dueDate} = todoObject;
 
         const html = `
-            <div>${name}</div>
+            <div>${name}</div>     
             <div>${dueDate}</div>
             
-        <button onclick="todoList.splice(${index00}, 1);
-            renderTodoList()" class="delete-todo"
+        <button class="delete-todo js-delete-todo"
             >Delete</button>`;
 
         todoListHTML += html;
     })
+
+
+
+
     /*for (let i = 0; i < todoList.length; i++) {
         const todoObject = todoList[i];
         // const name = todoObject.name;
@@ -38,7 +41,19 @@ function renderTodoList () {
 
     document.querySelector('.js-todo-list')
         .innerHTML = todoListHTML;
+    document.querySelectorAll('.js-delete-todo')
+        .forEach((deleteButton, index)=> {
+            deleteButton.addEventListener('click', (e) => {
+                todoList.splice(index, 1);
+                renderTodoList();
+            });
+        });
 }
+
+document.querySelector('.js-add-todo')
+    .addEventListener('click', ()=> {
+    addTodo();
+});
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
